23755199d216fd5ac121eeb94da98ad53aeceddf
Dawid Dziurdzia <ddziurdzia@me.com>
2013-07-13 21:34:44 +0200

    Haversine distance algorithm

--- lib/gpx/haversine.rb
+++ lib/gpx/haversine.rb
@@ -1,7 +1,23 @@
 module GPX
 	class Haversine
+		RADIAN_PER_DEGREE = Math::PI/180.0
+		EARTH_RADIUS = 6371.00079
+		
 		def self.distance(latitudeA, longitudeA, latitudeB, longitudeB)
-			17.47
+			latitudeA_rad = latitudeA*RADIAN_PER_DEGREE
+			latitudeB_rad = latitudeB*RADIAN_PER_DEGREE
+			
+			distance_latitude = (latitudeB-latitudeA) * RADIAN_PER_DEGREE
+			distance_longitude = (longitudeB-longitudeA) * RADIAN_PER_DEGREE
+			
+			a = (Math::sin(distance_latitude/2.0)**2.0) +
+						Math::cos(latitudeA_rad) * Math::cos(latitudeB_rad) * (Math::sin(distance_longitude/2.0)**2.0)
+			c = 2.0 * Math::atan2(Math::sqrt(a), Math::sqrt(1.0-a))
+			
+			distance = EARTH_RADIUS * c
+			#ele = (ele1/1000.0-ele2/1000.0).abs
+			
+			#Math::sqrt(distance*distance + ele*ele)
 		end
 	end
 end
--- spec/lib/haversine_spec.rb
+++ spec/lib/haversine_spec.rb
@@ -1,14 +1,48 @@
 require 'spec_helper'
 
+PRECISION = 0.002 # 0.01%
+
 describe GPX do
-	let(:zabrze_lat) { 18.7833 }
-	let(:zabrze_lon) { 50.3167 }
-	let(:katowice_lat) { 19.0167 }
-	let(:katowice_lon) { 50.2667 }
+	def should_equals_distance_with_precision(checked_distance, real_distance)
+    (checked_distance-real_distance).abs.should < real_distance * PRECISION
+  end
+	
+	let(:zabrze_lon) { 18.7833 }
+	let(:zabrze_lat) { 50.3167 }
+	let(:katowice_lon) { 19.0167 }
+	let(:katowice_lat) { 50.2667 }
 	let(:katowice_distance) { 17.47 }
 	
-	it "should return proper distance for two values" do
+	it "should return proper distance from Zabrze, PL to Katowice, PL" do
 		distance = GPX::Haversine.distance(zabrze_lat, zabrze_lon, katowice_lat, katowice_lon)
-		distance.should eq katowice_distance
+		should_equals_distance_with_precision(distance, katowice_distance)
 	end
+	
+	it "should return proper distance from Katowice, PL to Zabrze, PL" do
+		distance = GPX::Haversine.distance(katowice_lat, katowice_lon, zabrze_lat, zabrze_lon)
+		should_equals_distance_with_precision(distance, katowice_distance)
+	end
+	
+	let(:tokyo_lon) { 139.7514 }
+	let(:tokyo_lat) { 35.6850 }
+	let(:london_lon) { -0.1167 }
+	let(:london_lat) { 51.5000 }
+	let(:london_distance) { 9554.72 }
+  
+  it "should return proper distance from Tokyo, JP to London, GB" do
+  	distance = GPX::Haversine.distance(tokyo_lat, tokyo_lon, london_lat, london_lon)
+  	should_equals_distance_with_precision(distance, london_distance)
+  end
+
+	it "should return proper length of half of equinox" do
+		distance = GPX::Haversine.distance(0,0,0,180)
+		should_equals_distance_with_precision(distance, 20038)
+	end
+  
+  let(:same_point) { 44.654 }
+  it "should return 0 for same lat/lon" do
+  	distance = GPX::Haversine.distance(same_point,same_point,same_point,same_point)
+  	distance.should eq 0
+  end
+  
 end
